28cba2ba416cbda7e476cb5deb8cce7a8367fe44
albertkipchirchir <albert@eldohub.co.ke>
2023-03-24 06:46:05 +0300

    code clean up

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+let storedProducts = JSON.parse(localStorage.getItem("products"))
+let productsContainer = document.querySelector(".products")
+
 function renderProduct(desc, imglink, id){
     let productCard = document.createElement("div")
-    productCard.classList.add("product")
     let imageEl = document.createElement("img")
+    let descEl = document.createElement("p")
+    let delButton = document.createElement("button")
+
+    productCard.classList.add("product")
     imageEl.setAttribute("src", imglink)
     imageEl.setAttribute("alt", desc)
-    let descEl = document.createElement("p")
     descEl.textContent = desc
-
-    let delButton = document.createElement("button")
     delButton.textContent = "delete"
     delButton.setAttribute("id", id)
     delButton.setAttribute("class", "delete-btn")
@@ -16,19 +19,8 @@ function renderProduct(desc, imglink, id){
     productCard.append(descEl)
     productCard.append(delButton)
 
-    document.querySelector(".products").append(productCard)
+    productsContainer.append(productCard)
 }
-
-let prods = JSON.parse(localStorage.getItem("products"))
-// console.log(products)
-function renderProducts(products){
-    document.querySelector(".products").innerHTML = ""
-    products.forEach(prod=>{
-        renderProduct(prod.des, prod.img, prod.id)
-    })
-}
-
-renderProducts(prods)
 
 function addEvents(){
     document.querySelectorAll(".delete-btn").forEach(btn=>{
@@ -38,26 +30,48 @@ function addEvents(){
         })
 })
 }
-addEvents()
 
-
-function deleteProduct(id){
-    let filteredProducts = prods.filter(product=> product.id !== id )
-    localStorage.clear()
-    localStorage.setItem("products", JSON.stringify(filteredProducts)) 
-    prods = JSON.parse(localStorage.getItem("products"))
-    renderProducts(prods)
+function renderProducts(products){
+    productsContainer.innerHTML = ""
+    products.forEach(product=>{
+        renderProduct(product.des, product.img, product.id)
+    })
     addEvents()
 }
 
-// form
 
-function addProduct(){
-    // array.push() to add new object(product) to products arr
-    // update the local storage
-    // render products from updated local strage
-    // addEvents()
+function deleteProduct(id){
+    let filteredProducts = JSON.parse(localStorage.getItem("products")).filter(product=> product.id != id )
+    localStorage.clear()
+    localStorage.setItem("products", JSON.stringify(filteredProducts)) 
+    renderProducts(filteredProducts)
 }
 
+// form
+function addProduct(desc, link){
+    let uniqueID = Date.now() + Math.floor(Math.random()*9999999)
+    let newProduct = {
+        "id": uniqueID,
+        "des": desc,
+        "img": link
+    }
+    let currentProducts = JSON.parse(localStorage.getItem("products")) // existing products
+    // array.push() to add new object(product) to existing products arr
+    currentProducts.push(newProduct)
+    // update the local storage
+    localStorage.clear()
+    localStorage.setItem("products", JSON.stringify(currentProducts))
+    // render products from updated local strage
+    renderProducts(currentProducts)
+}
 
+document.getElementById("new-product-form").addEventListener("submit", (e)=>{
+        e.preventDefault()
+        let descInput = document.getElementById("desc")
+        let linkInput = document.getElementById("link")
+        addProduct(descInput.value, linkInput.value)
+        descInput.value = ""
+        linkInput.value = ""
+})
 
+renderProducts(storedProducts)
